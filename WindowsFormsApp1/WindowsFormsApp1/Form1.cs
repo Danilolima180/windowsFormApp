c09e524d04688b0f6ca8ed51c1a5ccb7c0e9d6a8
Danilo Rodrigues De Lima <38114256+Danilolima180@users.noreply.github.com>
2022-12-07 11:22:36 -0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible= false;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+            label2.Text = "Texto apaga";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           label2.Visible=false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible= true;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+         
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
